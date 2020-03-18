148e6925f8e5f74f1917c39717b75c6128ff570f
Kalyani Tiwari <kalyani7t@gmail.com>
2020-03-18 17:49:40 +0530

    Added BCryptEncoder bean and excluded SpringAutoConfiguration.class

--- src/main/java/com/example/demo/eCommerceApp.java
+++ src/main/java/com/example/demo/eCommerceApp.java
@@ -3,12 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
-@SpringBootApplication
+//Here we are excluding the default SecurityAutoConfiguration class provided by Spring Boot as we are using JWT implementation
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class eCommerceApp {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(eCommerceApp.class, args);
